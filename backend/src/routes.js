b4d44735bdfea0bd62b16e9410a4b262088d3200
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-03-25 15:00:17 -0300

    add knex

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const app = express();
+const routes = express.Router();
 
-app.get('/', (request, response) => {
+routes.get('/', (request, response) => {
 	return response.json({
 		evento: "Semana Omnistack 11",
 		aluno: "Tarcisio Coutinho"
 	});
 });
 
-app.listen(3333);
+module.exports = routes;
