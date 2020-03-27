532c91717ac4a02a36d65136d260719fa727057b
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-03-27 12:11:24 -0300

    Ajuste na consulta de incidentes do profile no backend Finalização do frontend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -20,7 +20,7 @@ module.exports = {
 
 		const incidents = await connection('incidents')
 			.select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf'])
-			.join('ongs', 'ong_id', '=', 'incidents.ong_id')
+			.join('ongs', 'ongs.id', '=', 'incidents.ong_id')
 			.limit(5)
 			.offset((page - 1) * 5);
 
