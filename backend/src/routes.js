1c997810478c913ee690613f9350938989b33216
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-03-28 11:11:53 -0300

    Start validations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -7,7 +9,15 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+	[Segments.BODY]: Joi.object().keys({
+		name: Joi.string().required(),
+		email: Joi.string().required().email(),
+		whatsapp: Joi.number().required().min(10).max(15),
+		city: Joi.string().required(),
+		uf: Joi.string().length(2)
+	})
+}), OngController.create);
 
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
